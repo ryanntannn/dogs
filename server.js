0afc672f2fd11421a558590d72a9e1084291f809
Depressionn <tantiongguan123@gmail.com>
2020-12-29 10:43:14 +0800

    Node stuff

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var app = express();
+
+// must specify options hash even if no options provided!
+var phpExpress = require('php-express')({
+  // assumes php is in your PATH
+  binPath: 'C:/php/php.exe'
+});
+
+// set view engine to php-express
+app.set('views', './src');
+app.engine('php', phpExpress.engine);
+app.set('view engine', 'php');
+
+// routing all .php file to php-express
+app.all(/.+\.php$/, phpExpress.router);
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log(`PHPExpress app listening at http://${host}:${port}`);
+});
+
+app.get('/', (res, req)=>{
+    req.render('index.php');
+});
