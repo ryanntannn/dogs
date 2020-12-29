6050b6a6f1a14f7059d2c251894ae6d855bb1b7c
Ryan <tanryan777@gmail.com>
2020-12-29 14:51:53 +0800

    add bars

--- src/index_scripts.js
+++ src/index_scripts.js
@@ -1,6 +1,10 @@
 var socket = io();
 var stats = document.querySelector('.stats-pane');
 
+var hungerBar = document.getElementById('hunger-bar');
+var happinessBar = document.getElementById('happiness-bar');
+var cleanlinessBar = document.getElementById('cleanliness-bar');
+
 function feedDog(){
     socket.emit('update dog', {id: 0, type: 'feed'});
 }
@@ -14,5 +18,8 @@ function cleanDog(){
 }
 
 socket.on('update stats', res =>{
-    stats.innerHTML = `hunger: ${res.hunger}, happiness: ${res.happiness}, cleanlines: ${res.cleaniness}`;
+	// stats.innerHTML = `hunger: ${res.hunger}, happiness: ${res.happiness}, cleanlines: ${res.cleaniness}`;
+	hungerBar.style.width = `${res.hunger*100.0}%`
+	happinessBar.style.width = `${res.happiness*100.0}%`
+	cleanlinessBar.style.width = `${res.cleaniness*100.0}%`
 });
